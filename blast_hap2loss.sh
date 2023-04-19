afbb930b38e04be68163585e0b4ef13b3858b99f
bmsacchi <bianca.sacchi@mail.utoronto.ca>
2023-04-19 13:01:20 -0400

    blast

--- blast_hap2loss.sh
+++ blast_hap2loss.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 awk -F"-" '/^>/{print $1; next}1' NChap1_final.all.maker.transcripts.fasta > NChap1.out.fasta
 
-
+awk '{print $2}' genelisthap1.txt > genelisthap1.out.txt 
 echo "Filter out candidate loss genes from nchap1 transcript fasta"
-genelist="genelisthap1.txt"
+genelist="genelisthap1.out.txt"
 fasta="NChap1.out.fasta"
 while read i; do
     samtools faidx $fasta $i  >> hap1_sequences.fa
@@ -11,13 +11,13 @@ done < $genelist
 
 wait
 
-#echo "starting blastn"
-#makeblastdb -in NChap2_final.all.maker.transcripts.fasta -dbtype nucl \
-#  -input_type fasta -out hap2transcripts
+echo "starting blastn"
+makeblastdb -in NChap2_final.all.maker.transcripts.fasta -dbtype nucl \
+  -input_type fasta -out hap2transcripts
   
-#wait
+wait
 
 
 
-#blastn -db hap2transcripts -query hap1_sequences.fasta \
-#  -out blastout.txt
+blastn -db hap2transcripts -query hap1_sequences.fa \
+  -out blastout.txt
