f9dbf1c097e7d30d7aa297054799b4f32093793a
bmsacchi <bianca.sacchi@mail.utoronto.ca>
2023-04-19 11:09:54 -0400

    parsing fasta headers

--- blast_hap2loss.sh
+++ blast_hap2loss.sh
@@ -1,1 +1,23 @@
-!/bin/bash
+#!/bin/bash
+awk -F"-" '/^>/{print $1; next}1' NChap1_final.all.maker.transcripts.fasta > NChap1.out.fasta
+
+
+echo "Filter out candidate loss genes from nchap1 transcript fasta"
+genelist="genelisthap1.txt"
+fasta="NChap1.out.fasta"
+while read i; do
+    samtools faidx $fasta $i  >> hap1_sequences.fa
+done < $genelist
+
+wait
+
+#echo "starting blastn"
+#makeblastdb -in NChap2_final.all.maker.transcripts.fasta -dbtype nucl \
+#  -input_type fasta -out hap2transcripts
+  
+#wait
+
+
+
+#blastn -db hap2transcripts -query hap1_sequences.fasta \
+#  -out blastout.txt
